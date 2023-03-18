3976e6eaf2272bfbe92f34f4393da52def9b46ab
¨isi.mube¨ <isi.mube@protonmail.com>
2023-03-18 17:18:11 +0100

    up2date

---
+++ lab_sql_self_and_cross_join_imb_solution.sql
@@ -0,0 +1,21 @@
+############################################
+################# UNIT | 3 ################# 
+############################################
+####### LAB | SQL | SELF&Cross JOIN ######## 
+############################################
+
+USE sakila;
+
+# 01. Get all pairs of actors that worked together.
+	-- Which tables?
+	-- Which keys?
+    
+# 02. Get all pairs of actors that worked together.
+	-- Which tables?
+	-- Which keys?
+    
+# 03. Get all pairs of actors that worked together.
+	-- Which tables?
+	-- Which keys?
+    
+-- IMB_solution
